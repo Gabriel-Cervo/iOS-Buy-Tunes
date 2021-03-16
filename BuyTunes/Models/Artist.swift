0b34e60cfb698383f87c5e61a9759bf2f418fa22
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-03-16 16:25:19 -0300

    created models

---
+++ BuyTunes/Models/Artist.swift
@@ -0,0 +1,12 @@
+//
+//  Artist.swift
+//  BuyTunes
+//
+//  Created by Thaís Fernandes on 16/03/21.
+//
+
+struct Artist {
+    let name: String
+    let songs: Array<Song>
+    let description: String
+}
---
+++ BuyTunes/Models/Song.swift
@@ -0,0 +1,15 @@
+//
+//  Song.swift
+//  BuyTunes
+//
+//  Created by Thaís Fernandes on 16/03/21.
+//
+
+import Foundation
+
+struct Song {
+    let title: String
+    let artist: Artist
+    let releaseDate: Date
+    let price: Double
+}
